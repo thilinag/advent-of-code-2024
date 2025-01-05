5e2396b2db2c9ddf2d68c543c8db69a7df89747e
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2025-01-05 21:17:45 +1030

    day 22 part 2

--- day22.js
+++ day22.js
@@ -7,8 +7,11 @@ const part1Data = {
 };
 
 const part2Data = {
-    sample: ``,
-    answer: 0,
+    sample: `1
+2
+3
+2024`,
+    answer: 23,
 };
 
 const sampleData = [part1Data, part2Data];
@@ -38,10 +41,48 @@ const part1 = () => {
     return data.reduce((acc, cur) => acc + getSecret(cur), 0);
 };
 
+const getSecrets = (n, prices = []) => {
+    // add last digit of initial price
+    prices.push(n % 10);
+    for (let i = 0; i < 2000; i++) {
+        const step1 = (((n * 64) ^ n) >>> 0) % 16777216;
+        const step2 = ((Math.floor(step1 / 32) ^ step1) >>> 0) % 16777216;
+        n = (((step2 * 2048) ^ step2) >>> 0) % 16777216;
+        // add last digit after getting secret
+        prices.push(n % 10);
+    }
+    return prices;
+};
+
 const part2 = () => {
     const data = getData(2);
-    // part 2 code
-    // return ;
+    const totalBananasPerChange = new Map();
+    data.forEach((startPrice) => {
+        const sellingPrices = getSecrets(startPrice);
+        const changes = new Set();
+        sellingPrices.forEach((price, idx) => {
+            if (idx > 3) {
+                // get sequence of 4 differences
+                const key = `${
+                    sellingPrices[idx - 3] - sellingPrices[idx - 4]
+                },${sellingPrices[idx - 2] - sellingPrices[idx - 3]},${
+                    sellingPrices[idx - 1] - sellingPrices[idx - 2]
+                },${price - sellingPrices[idx - 1]}`;
+                // if we have't seen this sequence before
+                if (!changes.has(key)) {
+                    changes.add(key);
+                    // remember the bananas for this specific sequence
+                    totalBananasPerChange.set(
+                        key,
+                        (totalBananasPerChange.get(key) || 0) + price
+                    );
+                }
+            }
+        });
+    });
+
+    // find most number of bananas
+    return Math.max(...totalBananasPerChange.values());
 };
 
 console.time("part1");
